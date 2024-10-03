ce24adcb3f9e55a46ddfeb59bc9b44d4e8c7dd63
joernih <jorn.halvorsen@gmail.com>
2024-10-03 15:45:16 +0200

    abc

--- examples/r/003-reactivity/app.R
+++ examples/r/003-reactivity/app.R
@@ -1,100 +1,62 @@
+if (grepl("wasm", sessionInfo()[[2]])) {
+  # If the session info contains "wasm", install the package from the specified repository
+  webr::install("WASMP", repos = "https://joernih.github.io/WASMA/")
+  library("WASMP")
+  data_vk <- WASMP::nb_ts
+} else {
+  # If the session info does not contain "wasm", load the package from the local library
+  library("WASMP")
+  data_vk <- WASMP::nb_ts
+}
+Packages
 library(shiny)
-library(bslib)
-
-# Define UI for dataset viewer app ----
-ui <- page_sidebar(
-
-  # App title ----
-  title = "Reactivity",
-
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-
-    # Input: Text for providing a caption ----
-    # Note: Changes made to the caption in the textInput control
-    # are updated in the output area immediately as you type
-    textInput(
-      inputId = "caption",
-      label = "Caption:",
-      value = "Data Summary"
+library(dplyr)
+library(ggplot2)
+# Shiny
+ui <- fluidPage(
+  titlePanel("Valutakursutvikling for norske kroner"),
+  sidebarLayout(
+   sidebarPanel(
+       selectInput("veks","Valg av valuta",c("value_EUR","value_USD","value_SEK")),
+       dateInput("datef", "Velg fra dato:", value = "2020-01-01"),
+       dateInput("datet", "Velg til dato:", value = "2021-10-05"),
+       selectInput("frekv","Frekvens",c("Daglig","Månedlig","Årlig")),
     ),
-
-    # Input: Selector for choosing dataset ----
-    selectInput(
-      inputId = "dataset",
-      label = "Choose a dataset:",
-      choices = c("rock", "pressure", "cars")
-    ),
-
-    # Input: Numeric entry for number of obs to view ----
-    numericInput(
-      inputId = "obs",
-      label = "Number of observations to view:",
-      value = 10
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Tidserieplot",plotOutput("plot")),
+       tabPanel("Tabell",tableOutput("table"))
+            )
     )
-  ),
-
-  # Output: Formatted text for caption ----
-  h3(textOutput("caption", container = span)),
-
-  # Output: Verbatim text for data summary ----
-  verbatimTextOutput("summary"),
-
-  # Output: HTML table with requested number of observations ----
-  tableOutput("view")
+  )
 )
-
-# Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
-
-  # Return the requested dataset ----
-  # By declaring datasetInput as a reactive expression we ensure
-  # that:
-  #
-  # 1. It is only called when the inputs it depends on changes
-  # 2. The computation and result are shared by all the callers,
-  #    i.e. it only executes a single time
-  datasetInput <- reactive({
-    switch(
-      input$dataset,
-      "rock" = rock,
-      "pressure" = pressure,
-      "cars" = cars
-    )
+  res <- reactive({
+    veks <- input$veks
+    startd_i <- as.Date(input$datef)
+    startd_n <- as.Date(input$datet)
+    frekvs_i <- pmatch(input$frekv,c("Årlig","Månedlig","Daglig"))
+    # Melt
+    dtf <- data_vk[[frekvs_i]][[1]]
+    mvg <- dtf %>% dplyr::mutate(date=base::as.Date(dato)) %>%
+      # Filter out all observations starting earlier or equal than startd_i
+      dplyr::filter(date>startd_i) %>%
+      # Filter out all observations starting later or equal than startd_n
+      dplyr::filter(date<startd_n) 
+    list(mvg,veks)
   })
+  output$plot <- renderPlot({
+    ggplot2::ggplot(res()[[1]], aes(x = date, y = !!sym(res()[[2]]))) + 
+      ggplot2::geom_point() +
+      ggplot2::labs(x = "Date", y = "Exchange Rate", color = "Currency") +
+      ggplot2::theme_classic()
 
-  # Create caption ----
-  # The output$caption is computed based on a reactive expression
-  # that returns input$caption. When the user changes the
-  # "caption" field:
-  #
-  # 1. This function is automatically called to recompute the output
-  # 2. New caption is pushed back to the browser for re-display
-  #
-  # Note that because the data-oriented reactive expressions
-  # below don't depend on input$caption, those expressions are
-  # NOT called when input$caption changes
-  output$caption <- renderText({
-    input$caption
   })
-
-  # Generate a summary of the dataset ----
-  # The output$summary depends on the datasetInput reactive
-  # expression, so will be re-executed whenever datasetInput is
-  # invalidated, i.e. whenever the input$dataset changes
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-
-  # Show the first "n" observations ----
-  # The output$view depends on both the databaseInput reactive
-  # expression and input$obs, so it will be re-executed whenever
-  # input$dataset or input$obs is changed
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
+  output$table <- renderTable({
+    res()[[1]]
   })
 }
+shinyApp(ui = ui, server = server)
 
-# Create Shiny app ----
-shinyApp(ui, server)
+
+
