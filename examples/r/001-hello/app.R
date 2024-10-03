dbe5945e1ba78399846eb9fd0058672a0aa8b4df
joernih <jorn.halvorsen@gmail.com>
2024-10-03 15:28:09 +0200

    treapps

--- examples/r/001-hello/app.R
+++ examples/r/001-hello/app.R
@@ -1,54 +1,69 @@
+if (grepl("wasm", sessionInfo()[[2]])) {
+  # If the session info contains "wasm", install the package from the specified repository
+  webr::install("WASMP", repos = "https://joernih.github.io/WASMA/")
+  library("WASMP")
+} else {
+  # If the session info does not contain "wasm", load the package from the local library
+  library("WASMP")
+  data_ma <- fred_ts[[2]]
+  vd1 <- as.Date("2022-01-01")
+  vd2 <- as.Date("2020-02-01")
+  plnv <- c('nvalue','growth')[1]
+}
+# Packages
 library(shiny)
-library(bslib)
-
-# Define UI for app that draws a histogram ----
-ui <- page_sidebar(
-
-  # App title ----
-  title = "Hello Shiny!",
-
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-
-    # Input: Slider for the number of bins ----
-    sliderInput(
-      inputId = "bins",
-      label = "Number of bins:",
-      min = 1,
-      max = 50,
-      value = 30
+library(dplyr)
+library(ggplot2)
+ui <- fluidPage(
+  titlePanel("Tidsserieutvikling for makroøkonomisk variabler før, under og etter pandemien"),
+  sidebarLayout(
+   sidebarPanel(
+      # Add any input options here if needed
+       selectInput("cntr","Land",c("USA","JPN","EUZ","GBR","CAN","NOR","DEN","SWE","SKE")),
+       selectInput("vars","Makrovariabel",c("mb3","cpi","une","mba","int","gdp")),
+       selectInput("veks","Type",c("nvalue","lnalue","lvalue","growth")),
+       dateInput("datef", "Valg fra dato:", value = "2018-01-01"),
+       dateInput("datet", "Valg til dato:", value = "2024-01-01")
+    ),
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Tidsserieplot",plotOutput("plot")),
+       tabPanel("Tidsseriedata",tableOutput("table"))
+            )
     )
-  ),
-
-  # Output: Histogram ----
-  plotOutput(outputId = "distPlot")
+  )
 )
-
-# Define server logic required to draw a histogram ----
 server <- function(input, output) {
-
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
-    x <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    hist(
-      x,
-      breaks = bins,
-      col = "#75AADB",
-      border = "white",
-      xlab = "Waiting time to next eruption (in mins)",
-      main = "Histogram of waiting times"
-    )
+  res <- reactive({
+    cntr <- input$cntr
+    varv <- input$vars
+    veks <- input$veks
+    startd_i <- as.Date(input$datef)
+    startd_n <- as.Date(input$datet)
+    #c("mb3"="Money","cpi"="Consumer","une"="Unemployment","mba"="Mba","int"="Interest rate","gdp"="GDB")
+    dfg <- dplyr::filter(data_ma,id==varv,country%in%cntr) %>%
+      dplyr::mutate(date=base::as.Date(date)) %>%
+      dplyr::filter(date>startd_i) %>%
+      dplyr::filter(date<startd_n) %>%
+      dplyr::mutate(nvalue=100*value/value[1]) %>%
+      dplyr::mutate(lnalue=log(value)) %>% 
+      dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>% 
+      dplyr::mutate(growth=round(value/lvalue-1, 6)*100) 
+    mv <- mean(dfg[[veks]], na.rm=TRUE)
+    list(dfg,mv,cntr,veks)
+  })
+  output$plot <- renderPlot({
+    ggplot(res()[[1]],aes(x = date, y =!!as.name(res()[[4]]))) + 
+    geom_point() +
+    geom_vline(xintercept = as.numeric(vd1), linetype = "dashed", color = "blue") +
+    geom_vline(xintercept = as.numeric(vd2), linetype = "dashed", color = "blue") +
+    geom_hline(yintercept = res()[[2]], linetype = "dashed", color = "red") +
+    theme_classic()
+  })
+  output$table <- renderTable({
+    dplyr::select(res()[[1]],date,growth,country,nvalue,lnalue)
   })
 }
-
-# Create Shiny app ----
 shinyApp(ui = ui, server = server)
+
+
--- examples/r/002-text/app.R
+++ examples/r/002-text/app.R
@@ -1,61 +1,71 @@
+if (grepl("wasm", sessionInfo()[[2]])) {
+  # If the session info contains "wasm", install the package from the specified repository
+  webr::install("WASMP", repos = "https://joernih.github.io/WASMA/")
+  library("WASMP")
+} else {
+  # If the session info does not contain "wasm", load the package from the local library
+  library("WASMP")
+  vd1 <- as.Date("2022-01-01")
+  vd2 <- as.Date("2020-02-01")
+  plnv <- c('nvalue','growth')[1]
+  data_ma <- fred_ts[[2]]
+}
+# Packages
 library(shiny)
-library(bslib)
-
-# Define UI for dataset viewer app ----
-ui <- page_sidebar(
-
-  # App title ----
-  title = "Shiny Text",
-
-  # Sidebar panel for inputs ----
-  sidebar = sidebar(
-
-    # Input: Selector for choosing dataset ----
-    selectInput(
-      inputId = "dataset",
-      label = "Choose a dataset:",
-      choices = c("rock", "pressure", "cars")
+library(dplyr)
+library(ggplot2)
+# Shiny
+ui <- fluidPage(
+  titlePanel("Tidsserieutvikling for makroøkonomisk variabler for mange land før, under og etter pandemien"),
+  sidebarLayout(
+   sidebarPanel(
+       selectizeInput("cntr","Valg av land",choices=c("USA","JPN","EUZ","GBR","CAN","NOR","DEN","SWE","SKE"),
+		      multiple =TRUE,options=list(maxItems=4), selected=c("NOR")),
+       selectInput("vars","Makroøknomisk variabel",c("mb3","cpi","une","mba","int","gdp")),
+       selectInput("veks","Type",c("nvalue","growth")),
+       dateInput("datef", "Valg fra dato:", value = "2018-01-01"),
+       dateInput("datet", "Valg til dato:", value = "2024-01-01")
     ),
-
-    # Input: Numeric entry for number of obs to view ----
-    numericInput(
-      inputId = "obs",
-      label = "Number of observations to view:",
-      value = 10
+   mainPanel(
+     tabsetPanel(
+       tabPanel("Plot",plotOutput("plot")),
+       tabPanel("Tidsserier",tableOutput("table"))
+            )
     )
-  ),
-
-  # Output: Verbatim text for data summary ----
-  verbatimTextOutput("summary"),
-
-  # Output: HTML table with requested number of observations ----
-  tableOutput("view")
+  )
 )
-
-# Define server logic to summarize and view selected dataset ----
 server <- function(input, output) {
-
-  # Return the requested dataset ----
-  datasetInput <- reactive({
-    switch(
-      input$dataset,
-      "rock" = rock,
-      "pressure" = pressure,
-      "cars" = cars
-    )
+  res <- reactive({
+    cntr <- input$cntr
+    varv <- input$vars
+    veks <- input$veks
+    startd_i <- as.Date(input$datef)
+    startd_n <- as.Date(input$datet)
+    dfg <- dplyr::filter(data_ma,id==varv) %>%
+      dplyr::mutate(date=base::as.Date(date)) %>%
+      dplyr::filter(date>startd_i) %>%
+      dplyr::filter(date<startd_n) %>%
+      dplyr::group_by(country) %>%
+      dplyr::filter(country%in%cntr) %>%
+      dplyr::mutate(couid=paste0(country,id)) %>%
+      dplyr::mutate(nvalue=100*value/value[1]) %>%
+      dplyr::mutate(lnalue=log(value)) %>% 
+      dplyr::mutate(lvalue=dplyr::lag(value,n=12)) %>% 
+      dplyr::mutate(growth=round(value/lvalue-1, 6)*100) %>%
+      dplyr::ungroup() 
+    mv <- mean(dfg[[veks]], na.rm=TRUE)
+    list(dfg,mv,cntr,veks)
   })
-
-  # Generate a summary of the dataset ----
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
+  output$plot <- renderPlot({
+    ggplot2::ggplot(data=res()[[1]], aes(x = date, y =!!sym(res()[[4]]))) +
+      ggplot2::geom_line(aes(color = couid)) + 
+      ggplot2::labs(title='Tidsserier') +
+      geom_vline(xintercept = as.numeric(vd1), linetype = "dashed", color = "blue") +
+      geom_vline(xintercept = as.numeric(vd2), linetype = "dashed", color = "blue") +
+      ggplot2::theme_minimal()
   })
-
-  # Show the first "n" observations ----
-  output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
+  output$table <- renderTable({
+    dplyr::select(res()[[1]],date,growth,country,nvalue,lnalue)
   })
 }
-
-# Create Shiny app ----
 shinyApp(ui = ui, server = server)
